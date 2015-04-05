6edb60da9bd8b4f0b4149fee1be9311b81871801
Yves Mathieu <yvmath@gmail.com>
2015-04-05 20:02:07 +0200

    Put git config in .config/git + add a global gitignore

--- install_gitconfig.sh
+++ install_gitconfig.sh
@@ -2,9 +2,16 @@
 . "$(dirname $0)/func"
 
 echo "Install git configuration..."
-backup_copy ~/.gitconfig
+GITDIR=~/.config/git
+mkdir -p "$GITDIR"
 
-cat << EOF >> ~/.gitconfig
+if ! grep -q '\s*path\s*=\s*'"$DEVCONFDIR/etc/gitconfig" "$GITDIR/config"; then
+    backup_copy "$GITDIR/config"
+    cat << EOF >> "$GITDIR/config"
 [Include]
 	path = $DEVCONFDIR/etc/gitconfig
 EOF
+fi
+
+backup_move "$GITDIR/ignore"
+link etc/gitignore "$GITDIR/ignore"
